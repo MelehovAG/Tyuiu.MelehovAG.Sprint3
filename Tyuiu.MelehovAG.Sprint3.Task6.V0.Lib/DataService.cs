a6ced41c94e4cf1ea78541554053a9cc70c0fe81
Tyuiu.MelehovAG@outlook.com <dedmeleh@gmail.com>
2023-10-31 05:51:50 +0500

    Task5

--- Tyuiu.MelehovAG.Sprint3.Task6.V0.Lib/DataService.cs
+++ Tyuiu.MelehovAG.Sprint3.Task6.V0.Lib/DataService.cs
@@ -7,7 +7,23 @@ using tyuiu.cources.programming.interfaces.Sprint3;
 
 namespace Tyuiu.MelehovAG.Sprint3.Task6.V0.Lib
 {
-    public class DataService
+    public class DataService : ISprint3Task6V0
     {
+        public int GetSumTheDivisors(int startValue, int stopValue)
+        {
+            int x;
+            int sum = 0;
+            for (x = startValue; x <= stopValue; x++)
+            {
+                for (int d = 1; d <= x; d++)
+                {
+                    if (x % d == 0)
+                    {
+                        sum += d;
+                    }
+                }
+            }
+            return sum;
+        }
     }
 }
--- Tyuiu.MelehovAG.Sprint3.Task6.V0.Test/DataServiceTest.cs
+++ Tyuiu.MelehovAG.Sprint3.Task6.V0.Test/DataServiceTest.cs
@@ -10,6 +10,16 @@ namespace Tyuiu.MelehovAG.Sprint3.Task6.V0.Test
         [TestMethod]
         public void TestMethod1()
         {
+            DataService ds = new DataService();
+
+            int startValue = 10;
+            int stopValue = 15;
+
+            int res = ds.GetSumTheDivisors(startValue, stopValue);
+
+            int wait = 120;
+
+            Assert.AreEqual(wait, res);
         }
     }
 }
